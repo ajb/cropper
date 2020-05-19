e2ffa91d75eff93650a7adc82964f6d3400ad9df
Adam Becker <ad@mbecker.cc>
2020-05-19 12:21:49 -0700

    fix hold name duplication
    closes #3

--- src/utilities.js
+++ src/utilities.js
@@ -3,18 +3,23 @@ import { map, filter } from 'lodash'
 export function getIntersectionName(lines, lineIds) {
   let names = map(lineIds, (id) => lines[id].name)
 
-  let numberNames = filter(names, (name) => !!name.match(/[0-9]+/))
-  let letterNames = filter(names, (name) => !!name.match(/[A-Za-z]+/))
+  let numberNames = filter(names, (name) => !!name.match(/^[0-9]+$/))
+  let letterNames = filter(names, (name) => !!name.match(/^[A-Za-z]+$/))
 
-  return [
-    ...letterNames,
-    ...numberNames
-  ].join('')
+  if (numberNames.length && letterNames.length) {
+    return [
+      ...letterNames,
+      ...numberNames
+    ].join('')
+  } else {
+    return names.join(',')
+  }
+
 }
 
 export function nextNameInSequence(type, allNames) {
   if (type === 'alphabetical') {
-    let allAlphabeticalNames = filter(allNames, (name) => name.match(/[A-Z]/)).sort().reverse()
+    let allAlphabeticalNames = filter(allNames, (name) => name.match(/^[A-Z]$/)).sort().reverse()
 
     if (allAlphabeticalNames.length) {
       return String.fromCharCode(allAlphabeticalNames[0].charCodeAt(0) + 1)
@@ -22,7 +27,7 @@ export function nextNameInSequence(type, allNames) {
       return 'A'
     }
   } else if (type === 'numeric') {
-    let allNumericNames = filter(allNames, (name) => name.match(/[0-9]+/)).sort().reverse()
+    let allNumericNames = filter(allNames, (name) => name.match(/^[0-9]+$/)).sort().reverse()
 
     if (allNumericNames.length) {
       return (parseInt(allNumericNames[0], 10) + 1).toString()
